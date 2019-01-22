35dc18d7967df56474bd2dcaf44215561bde5102
William Robert Simpson <will.simpson85@gmail.com>
2019-01-22 21:49:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
 def my_select(collection)
  if block_given?
    i = 0
-   newarr = []
-   collection.each {|x| newarr << yield(x)}
-     newarr
+   
+   collection.each {|x|  yield(x)}
+
    end
 
 end
