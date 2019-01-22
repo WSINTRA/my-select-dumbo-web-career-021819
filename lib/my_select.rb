98f49305271b3631aadc37159146bd1ced89a6f8
William Robert Simpson <will.simpson85@gmail.com>
2019-01-22 21:55:33 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
  if block_given?
    collection.each do |x|
      yield x
-  
+
 
 end
 
@@ -10,4 +10,3 @@ end
 nums = [1, 2, 3, 4, 5]
 puts my_select(nums) do |num|
   num.even?
-end
