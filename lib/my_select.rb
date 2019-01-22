5d672f4f19539b52a4761d3360842440aee9254c
William Robert Simpson <will.simpson85@gmail.com>
2019-01-22 22:08:54 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,11 @@
 def my_select(collection)
  if block_given?
-   
- end
+   new_array = []
+     array.each do |item|
+       yield(item)
+       new_array << yield(item)
+     end
+     new_array
+   end
 
 end
